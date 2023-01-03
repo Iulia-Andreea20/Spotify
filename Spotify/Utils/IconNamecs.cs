140d14adaa630b63dc1876eb4b992f6c9db24866
Iulia-Andreea GRIGORE <iulia.grigore0201@stud.acs.upb.ro>
2023-01-03 21:39:36 +0200

    add the song bar

--- Spotify/Utils/IconNamecs.cs
+++ Spotify/Utils/IconNamecs.cs
@@ -11,5 +11,8 @@ namespace Spotify.Utils
         public static string Settings => ((char)0xe902).ToString();
         public static string Bell => ((char)0xe903).ToString();
         public static string Clock => ((char)0xe900).ToString();
+        public static string Pause => ((char)0xe901).ToString();
+        public static string Like => ((char)0xe914).ToString();
+        public static string MP3 => ((char)0xe91d).ToString();
     }
 }
